89ba354d9b9de85cf8c47b538a7a2e2f9de86ccc
Tom MacWright <tom@macwright.com>
2024-02-06 12:32:10 -0500

    docs: add TypeScript type definition

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import { FastifyPluginAsync } from 'fastify'
+
+type FastifyConstraints = FastifyPluginAsync<never>
+
+declare namespace fastifyConstraints {
+  export const fastifyConstraints: FastifyConstraints
+  export { fastifyConstraints as default }
+}
+
+declare function fastifyConstraints(
+  ...params: Parameters<FastifyConstraints>
+): ReturnType<FastifyConstraints>
+export = fastifyConstraints
